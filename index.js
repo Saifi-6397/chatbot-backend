e5c4679429dfa092fd617b1467e171a97a525917
Saifi-6397 <khaleelahmad3886@gmail.com>
2026-04-07 16:45:04 +0530

    set origin

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173" 
+  // origin: "http://localhost:5173" 
+  origin: "*"
 }));
 app.use(express.json());
 
