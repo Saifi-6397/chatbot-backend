65f34672e2978caa7ce7769d72d8828807c480a3
Saifi-6397 <khaleelahmad3886@gmail.com>
2026-04-08 12:05:27 +0530

    set frontend origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(cors({
   // origin: "http://localhost:5173" 
-  origin: "*"
+  origin: "https://ecommchatbot.netlify.app"
 }));
 app.use(express.json());
 
